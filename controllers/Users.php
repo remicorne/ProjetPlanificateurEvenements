e121cbc197fa801ae8b08843ea7ced75635f14a1
Mathieu <mathieu.skok@gmail.com>
2020-03-23 17:08:31 +0100

    creation de la variable mailer dans la class model

--- controllers/Users.php
+++ controllers/Users.php
@@ -13,19 +13,17 @@ class Users extends Controller {
   public function send_reset_password()// IMPROVE utiliser un lien de réinitialisation (nécessite un changement de la BD)
   { 
       try {
-          require "assets/PHPMailer/create_mailer.php"; //construit un objet de type mailer dans $mailer selon le process indiqué dans la doc
+          //require "assets/PHPMailer/create_mailer.php"; //construit un objet de type mailer dans $mailer selon le process indiqué dans la doc
           $email = filter_input(INPUT_POST, 'email');
           $user = $this->users->user_from_email($email); //trouve le user 
           if ($user == null) throw new Exception ('Pas de compte associé à cet e-mail');
           $name = "$user->prenom $user->nom"; //construit le nom du user
           $password = $this->users->reset_password($user->numUser);
-          $this->mailer->build_password_reset_email($mailer, $email, $name, $password); //construit l'email à envoyer et retourne le nouveau mdp
-          $this->mailer->send_email($mailer);
+          $this->mailer->build_password_reset_email($email, $name, $password); //construit l'email à envoyer et retourne le nouveau mdp
+          $this->mailer->send_email();
           header('Location: /index.php/sessions/sessions_new');
       } catch (Exception $e) {
-          var_dump($e->getMessage());
-          $this->loader->load('password_forgotten', ['title'=>'Votre adresse email de récupération',
-        'error_message' => $e->getMessage()]);
+          $this->loader->load('password_forgotten', ['title'=>'Votre adresse email de récupération','error' => $e->getMessage()]);
       }
   }
 
--- models/Mailer_model.php
+++ models/Mailer_model.php
@@ -1,26 +1,21 @@
 <?php
-
 class Mailer_model extends Model{
-                                // l'envoie d'email se sert de phpMailer, une libraitrie qui sert à ca
-                                // pour installer cette librairie il faut composer, un outil qui sert à installer des librairies
-                                // TODO : send code, goto enter code page, if code right, change password page
 
-    public function build_password_reset_email($mailer, $email, $name, $password) 
+    public function build_password_reset_email($email, $name, $password) 
     {
         try {
-            $mailer->addAddress($email, $name);
+            $this->$mailer->addAddress($email, $name);
             $content = file_get_contents('assets/PHPMailer/emailHTML/password_reset.html');
-            $mailer->msgHTML("$content $password");
-            $mailer->Subject = "Réinitialisation mot de passe";
-            return $password;
+            $this->$mailer->msgHTML("$content $password");
+            $this->$mailer->Subject = "Réinitialisation mot de passe";
         } catch (Exception $e) {
             throw new Exception('Impossible de construire l\'email');
         }
     }
 
-    public function send_email($mailer) //pas de try/catch car si echec pas d'exception, renvoie false
+    public function send_email() //pas de try/catch car si echec pas d'exception, renvoie false
     {
-            if (!$mailer->send()) throw new exception ("Echec de l'envoi : $mailer->ErrorInfo");
+        if (!$this->$mailer->send()) throw new Exception ("Echec de l'envoi : $mailer->ErrorInfo");
     }
 
 }
