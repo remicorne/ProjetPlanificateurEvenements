73ec318bb0e89d6923ce0b4f2fccd8513790dbfa
Mathieu <mathieu.skok@gmail.com>
2020-03-26 10:23:57 +0100

    modif non fichier

--- views/ajouter_participants_documents.php
+++ views/ajouter_participants_documents.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="/assets/js/script_gestion_des_participants.js"></script>
+<script type="text/javascript" src="/assets/js/script_participants_documents.js"></script>
 
 <div class="container">
   
